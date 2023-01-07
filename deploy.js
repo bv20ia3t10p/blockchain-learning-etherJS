27da689add9ebc951258c1023bc093a814c9ef75
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-01-06 17:32:30 -0800

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -6,7 +6,15 @@ const fs = require('fs-extra')
 require('dotenv').config()
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+  //   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+  // $PRIVATE_KEY = <address> node deploy.js
+  // This will deploy the contract with given private key and not having it inside any file
+  const encryptedJson = fs.readFileSync('./.encryptedKey.json', 'utf8')
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD,
+  )
+  wallet = await wallet.connect(provider)
   const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8')
   const binary = fs.readFileSync(
     './SimpleStorage_sol_SimpleStorage.bin',
